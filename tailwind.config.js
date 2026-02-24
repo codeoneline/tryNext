1d5094f93e70f1f128e53fd426b53aba4b3ed867
codeoneline <mutouji1@163.com>
2026-02-24 17:20:59 +0800

    add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const config = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
